c2644bb5c40a21981a935410fcbb20733505d70a
fangfubin <ffb@ngarihealth.com>
2020-12-31 16:02:49 +0800

    test

--- Chapter1/src/main/java/com/didispace/Chapter1Application.java
+++ Chapter1/src/main/java/com/didispace/Chapter1Application.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Chapter1Application {
 
+	/**
+	 * 测试
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Chapter1Application.class, args);
 	}
